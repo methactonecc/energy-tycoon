32e4f7d46c18595a8c9822682ecb823c4934ad05
Methacton Electric Car Club <methactonecc@gmail.com>
2014-05-13 11:48:00 -0400

    Moved relevant code to sidebarview.js

--- src/views/sidebarview.js
+++ src/views/sidebarview.js
@@ -1,6 +1,30 @@
 ET.SidebarView = Backbone.View.extend({
   /*
     Handles career stuff in the sidebar only.
+    
+    Model: career
   */
-  el: $('#sidebar')
+  el: $('#sidebar'),
+  
+  //Template functions to use
+statsTemplate: template("template-stats"),	//vital stats like money, power, etc.
+
+initialize: function(){
+//create throttled versions of commonly-called fns
+this.render = _.throttle(this._render, 500);
+
+//Bind to relevant events here
+     this.listenTo(this.model, "all", this.render);
+     this.listenTo(ET.plants, "all", this.render);
+     this.listenTo(ET.cities, "all", this.render);
+    
+     //Intercept region changes and update menu on the sidebar accordingly
+     //this.listenTo(ET,.appView.model, "change:viewType", this.renderMenu);
+},
+
+_render: function(){
+this.$('#sidebar-stats').html(this.statsTemplate({ stats: this.model, cities: ET.cities }));
+},
+
+
 });
